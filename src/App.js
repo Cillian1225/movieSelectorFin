6aa3977062a4ff0e01957cca7a34ed325443cfad
cillian Jung <kyn00018@gmail.com>
2022-07-15 00:08:43 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Button from "./Button";
+import React,{Component} from 'react';
+function App() {
+  return (
+    <div className="App">
+      <h3>welcome back</h3>
+      <Button text={"Continue"} />
+    </div>
+  );
+ 
+}
+
+export default App;
